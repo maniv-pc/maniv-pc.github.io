3396a8100fc1ed1f3c7d8d3e58534e7a1b7d6e7f
maniv-pc <maniv.pc.founder@gmail.com>
2024-12-10 08:26:32 +0200

    remove_app_js

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Ensure this points to `App.tsx`
+import "./index.css";
 
-const React = require("react");
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
